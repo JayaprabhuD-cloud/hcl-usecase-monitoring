c186bf43309a3e388b7714c71e591e5eb04efdae
JayaprabhuD-cloud <mail2jayaprabhu@gmail.com>
2025-06-17 05:27:01 +0530

    Fixing issue

--- provider.tf
+++ provider.tf
@@ -7,13 +7,13 @@ terraform {
   }
   backend "s3" {
     bucket = "bayer-new-terraform-remote-state-bucket"
-    key    = "usecase-8/terraform.tfstate"
-    region = "ap-south-1" 
+    key    = "usecase-11/terraform.tfstate"
+    region = "us-east-1" 
     use_lockfile = true   
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
