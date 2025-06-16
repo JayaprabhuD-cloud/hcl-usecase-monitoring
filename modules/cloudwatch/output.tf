934ea8047eb7d1c8cb3ce2c8077eff1492a83ede
JayaprabhuD-cloud <mail2jayaprabhu@gmail.com>
2025-06-17 04:23:35 +0530

    Fixing issuses

--- modules/cloudwatch/output.tf
+++ modules/cloudwatch/output.tf
@@ -1,10 +1,9 @@
 output "log_group_arn" {
-  description = "The ARN of the CloudWatch Log Group"
-  value       = aws_cloudwatch_log_group.cloudtrail_log_group.arn
+  description = "The ARN of the CloudWatch Log Group"
+  value       = aws_cloudwatch_log_group.cloudtrail_log_group.arn
 }
 
-
 output "cloudtrail_iam_role_arn" {
-  description = "The ARN of the IAM role"
-  value       = aws_iam_role.cloudtrail_role.arn
+  description = "The ARN of the IAM role used by CloudTrail"
+  value       = aws_iam_role.cloudtrail_role.arn
 }
