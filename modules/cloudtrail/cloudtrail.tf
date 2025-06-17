65655fe5e297990bd71c76f54875452a9a516c5f
JayaprabhuD-cloud <mail2jayaprabhu@gmail.com>
2025-06-17 10:20:30 +0530

    Good morning

--- modules/cloudtrail/cloudtrail.tf
+++ modules/cloudtrail/cloudtrail.tf
@@ -1,14 +1,15 @@
 # Creating multi region cloudtrail
-
-resource "aws_cloudtrail" "main" {
-  name                          = var.cloudtrail_name
-  s3_bucket_name                = var.cloudtrail_bucket_name
-  cloud_watch_logs_group_arn = aws_cloudwatch_log_group.cloudtrail_log_group.arn
-  cloud_watch_logs_role_arn    = aws_iam_role.cloudtrail_role.arn
+#
+#resource "aws_cloudtrail" "main" {
+#  name                          = var.cloudtrail_name
+#  s3_bucket_name                = var.cloudtrail_bucket_name
+#  cloud_watch_logs_group_arn = aws_cloudwatch_log_group.cloudtrail_log_group.arn
+#  cloud_watch_logs_role_arn    = aws_iam_role.cloudtrail_role.arn
 #  cloud_watch_logs_group_arn    = var.log_group_arn
 #  cloud_watch_logs_role_arn     = var.cloudtrail_iam_role_arn
-  include_global_service_events = true
-  is_multi_region_trail         = true
-  enable_log_file_validation    = true
-}
-
+#  include_global_service_events = true
+#  is_multi_region_trail         = true
+#  enable_log_file_validation    = true
+#}
+#
+#
