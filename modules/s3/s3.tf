69bead33579bff53e65da386d6062fba6e53c16b
JayaprabhuD-cloud <mail2jayaprabhu@gmail.com>
2025-06-17 11:28:28 +0530

    ss

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -5,3 +5,37 @@ resource "aws_s3_bucket" "cloudtrail_bucket" {
     Name = "var.cloudtrail_bucket_name"
   }
 }
+
+
+resource "aws_s3_bucket_policy" "cloudtrail_bucket_policy" {
+  bucket = var.cloudtrail_bucket_name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AWSCloudTrailAclCheck",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action    = "s3:GetBucketAcl",
+        Resource  = "arn:aws:s3:::${var.cloudtrail_bucket_name}"
+      },
+      {
+        Sid       = "AWSCloudTrailWrite",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action    = "s3:PutObject",
+        Resource  = "arn:aws:s3:::${var.cloudtrail_bucket_name}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
