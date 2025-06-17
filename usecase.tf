50781a68f9c7a64c442cdc3e879df4d75879eb48
JayaprabhuD-cloud <mail2jayaprabhu@gmail.com>
2025-06-17 11:12:24 +0530

    gg

--- usecase.tf
+++ usecase.tf
@@ -10,8 +10,6 @@ module "sns" {
 
 module "cloudwatch" {
   source      = "./modules/cloudwatch"
-  cloudtrail_log_group_name     = var.cloudtrail_log_group_name
-  retention = var.retention
   sns_topic_arn = [module.sns.sns_topic_arn]
   cloudtrail_name  = var.cloudtrail_name
   cloudtrail_bucket_name = var.cloudtrail_bucket_name
